89582b95848dd3588dacc5dc86c8266a8ab111b2
Luis Vergara <lvergararobledo@gmail.com>
2026-05-09 13:35:05 -0400

    fix: alinear stack con RN 0.81.5 + React 19 + expo-router 6 + reanimated 4 (igual que gestor_secplan)

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,9 +1,11 @@
-/** @type {import('@babel/core').TransformOptions} */
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+    ],
+    plugins: [
+      "react-native-reanimated/plugin",
     ],
   };
 };
